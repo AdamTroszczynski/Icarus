701183eb7fe98d379f07d7ddbc611dd05540c232
Amanowsky <adamtroszczynski@outlook.com>
2025-08-23 23:06:46 +0200

    feat: add api enums

---
+++ src/enums/ActionsEnum.ts
@@ -0,0 +1,9 @@
+export enum ActionsEnum {
+  LOGIN_USER = 'loginUser',
+  AUTO_LOGIN_USER = 'autoLoginUser',
+  START_TRAINING = 'startTraining',
+  START_TRAINING_QUEST = 'startTrainingQuest',
+  CLAIM_TRAINING_QUEST_REWARD = 'claimTrainingQuestRewards',
+  CLAIM_TRAINING_STAR = 'claimTrainingStar',
+  FINISH_TRAINING = 'finishTraining',
+}
---
+++ src/enums/AuthEnum.ts
@@ -0,0 +1,9 @@
+export enum AuthEnum {
+  autoLoginUser = '7623846950c392416578dd33010cc41c',
+  loginUser = 'b67c0311ba923f64edc58bf3d7957b60',
+  startTraining = '8a896c5e10deb2712c082c713f865c36',
+  startTrainingQuest = '5ae8f6357c8ceb4b9130b05506b8f453',
+  claimTrainingQuestRewards = 'beb8deadfe26aef457282967053f4343',
+  claimTrainingStar = '8a691b7bcf09de17085a3db110dd25a0',
+  finishTraining = 'e87271b83281f6c7be5b479291c81d3c',
+}
