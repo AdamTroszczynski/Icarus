b298db58fc240a669df63ee73ca2d668417e2eaf
Amanowsky <adamtroszczynski@outlook.com>
2025-08-24 00:58:17 +0200

    feat: clear app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,7 @@
 <template>
-  <input type="email" placeholder="Email" v-model="email" />
-  <input type="password" placeholder="Password" v-model="password" />
-  <button @click="loginAction">Login</button>
+  <RouterView />
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, type Ref } from 'vue';
-import { useAuth } from '@/composable/useAuth';
-
-const { login, autoLogin } = useAuth();
-
-const email: Ref<string> = ref('vmvdnkuvzejcznuila@nespf.com');
-const password: Ref<string> = ref('vmvdnkuvzejcznuila@nespf.com');
-
-const loginAction = async () => {
-  await login(email.value, password.value);
-};
-
-onMounted(() => {
-  autoLogin();
-});
+import { RouterView } from 'vue-router';
 </script>
