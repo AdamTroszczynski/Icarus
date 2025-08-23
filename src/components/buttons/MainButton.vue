ab89e6828e3c3528b9839b505bd68233f5c70004
Amanowsky <adamtroszczynski@outlook.com>
2025-08-24 00:56:55 +0200

    feat: add mainButton component

---
+++ src/components/buttons/MainButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button
+    class="flex w-full cursor-pointer items-center justify-center rounded-xl p-2 duration-200 active:scale-95"
+    :class="setBgColor"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+import { BgColorMap, type Colors } from '@/const/Colors';
+import { computed, type PropType } from 'vue';
+
+const props = defineProps({
+  bgColor: {
+    type: String as PropType<Colors>,
+    required: true,
+  },
+});
+
+const setBgColor = computed<string>(() => {
+  return BgColorMap[props.bgColor];
+});
+</script>
