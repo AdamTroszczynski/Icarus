eb5b6e14ad41e9a6b849568fa86cc21804341440
Amanowsky <adamtroszczynski@outlook.com>
2025-08-23 23:05:08 +0200

    feat: add starting project

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { fileURLToPath, URL } from 'node:url';
+
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueDevTools()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://pl35.herozerogame.com',
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // usuwa /api z początku
+      },
+    },
+  },
+});
